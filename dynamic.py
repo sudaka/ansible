e9341b15285c978bc5b153112c508bf3c18897e1
sergeyabobrov <bobrovsa@rambler.ru>
2022-07-12 15:13:53 +0800

    With checking init and struct

--- dynamic.py
+++ dynamic.py
@@ -2,8 +2,79 @@ import sys
 import argparse
 
 class Hostinfo():
-    def __init__(self, host='127.0.0.1') -> None:
+    def __init__(self, name='inname') -> None:
+        self.name = name
+    
+    def __str__(self) -> str:
+        props = {}
+        for (name,value) in self.__dict__.items():
+            if name.find('ansible')>-1:
+                props[name] = value
+        return str(props)
+
+    def getdict(self):
+        props = {}
+        for (name,value) in self.__dict__.items():
+            if name.find('ansible')>-1:
+                props[name] = value
+        return props
+
+    def sethost(self, host):
         self.ansible_ssh_host = host
+
+class Groupinfo():
+    def __init__(self, gname) -> None:
+        self.gname = gname
+        self.glist = []
+
+class Groups():
+    def __init__(self) -> None:
+        self.gdict = {}
+        self.hosts = {}
+
+    def addgroup(self, gname):
+        if gname not in self.gdict.keys():
+            self.gdict[gname] = []
+    
+    def delgroup(self, gname):
+        if gname in self.gdict.keys():
+            del self.gdict[gname]
+    
+    def addhost(self, tmphost=Hostinfo()):
+        if tmphost.name not in self.hosts.keys():
+            self.hosts[tmphost.name] = tmphost
+
+    def delhost(self, thost=Hostinfo()):
+        hostname = thost.name
+        if hostname in self.hosts.keys():
+            del self.hosts[hostname]
+            for tmpg in self.gdict.values():
+                if hostname in tmpg:
+                    tmpg.remove(hostname)
+
+    def gethost(self, hostname=''):
+        if hostname in self.hosts.keys():
+            return(self.hosts[hostname].getdict())
+        return ''
+
+    def addhostgroup(self, gname, tmphost=Hostinfo()):
+        self.addgroup(gname)
+        if tmphost.name not in self.gdict[gname]:
+            self.gdict[gname].append(tmphost.name)
+        self.addhost(tmphost=tmphost)
+
+    def delhostgroup(self, gname, tmphost=Hostinfo()):
+        if gname in self.gdict.keys():
+            if tmphost.name in self.gdict[gname]:
+                self.gdict[gname].remove(tmphost.name)
+    
+    def __str__(self) -> str:
+        outh = {}
+        for host in self.hosts.values():
+            outh[host.name] = host.getdict()
+        outf = dict(hostvars=outh)
+        out = dict(self.gdict, _meta=outf)
+        return str(out)
 
 def createParser ():
     parser = argparse.ArgumentParser()
@@ -17,15 +88,28 @@ def gethost(hostname):
     hostsinfo = {"main": hostname1, "sec": hostname2}
     hostinfo = hostsinfo.get(hostname)
     return hostinfo
- 
+
+def testinit():
+    m1 = Hostinfo(name='first')
+    m1.sethost('127.0.0.1')
+    m3 = Hostinfo(name='second')
+    m3.sethost('127.0.0.1')
+    m2 = Groups()
+    m2.addhostgroup('test1', m1)
+    m2.addhostgroup('test1', m3)
+    m2.addhostgroup('test2', m1)
+    m2.addhostgroup('test2', m3)
+    #m2.delhost(m1)
+    return m2
+
 if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
- 
-    #print (namespace)
- 
+    ghlist = testinit()
     if namespace.host:
-        hostinfo = gethost(namespace.host)
+        hostinfo = ghlist.gethost(namespace.host)
         print(hostinfo)
     if namespace.list:
-        print("list need")
+        print(ghlist)
+    
+    
